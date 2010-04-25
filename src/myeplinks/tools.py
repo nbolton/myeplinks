bf6dd59c0ad8094ed1837191f568245bd651353d
Nick Bolton <nick@synergy-project.org>
2010-04-25 22:54:37 +0000

    added logging (to console) with -d arg, and fixed procrec feature

--- src/myeplinks/tools.py
+++ src/myeplinks/tools.py
@@ -1,7 +1,43 @@
-import libxml2, httplib, re, os, urllib
+import libxml2, httplib, re, os, urllib, logging
 from xml.dom import minidom
 
+class LoggerFactory:
+	LOG_LEVELS = {
+		'debug' : logging.DEBUG,
+		'info' : logging.INFO,
+		'warning' : logging.WARNING,
+		'error' : logging.ERROR,
+		'critical' : logging.CRITICAL
+	}
+	
+	def create(self, logLevel):
+		# get level number from text name
+		levelNum = self.LOG_LEVELS.get(logLevel, logging.INFO)
+		
+		# create logger
+		log = logging.getLogger()
+		log.setLevel(levelNum)
+		
+		# create formatter, add formatter to ch, add ch to logger
+		formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+		
+		# console logger
+		ch = logging.StreamHandler()
+		ch.setLevel(levelNum)
+		ch.setFormatter(formatter)
+		log.addHandler(ch)
+		
+		log.debug("logging setup complete")
+		
+		return log
+	
+	def empty(self):
+		return logging.getLogger()
+
 class HttpDownloader:
+	def __init__(self, log=LoggerFactory().empty()):
+		self.log = log
+	
 	def download(self, url):
 		# we alreayd know we're using http, so discard the protocol
 		cleanUrl = url.replace('http://', '')
@@ -12,10 +48,14 @@ class HttpDownloader:
 		# the path is all the remaining parts
 		path = '/' + '/'.join(cleanUrl.split('/')[1:])
 		
+		self.log.debug('downloading from: %s' % url)
+		
 		# download the feed using GET
 		http = httplib.HTTPConnection(host)
 		http.request('GET', path)
 		resp = http.getresponse()
+		
+		self.log.debug('got status: %i', resp.status)
 		
 		if resp.status != 200:
 			raise Exception('Unable to download: %i %s' % (resp.status, resp.reason))
@@ -59,7 +99,8 @@ class TargetInfo:
 		return string
 
 class TargetExtractor:
-	def __init__(self, xpath, trim=None, split=None, regex=None):
+	def __init__(self, xpath, trim=None, split=None, regex=None, log=LoggerFactory().empty()):
+		self.log = log
 		self.xpath = xpath
 		self.trim = trim
 		self.split = split
@@ -70,6 +111,7 @@ class TargetExtractor:
 		# target, we create a TargetInfo object and return them as a 
 		# collection (each of which could represent a link for example)
 		doc = libxml2.parseDoc(feedXml)
+		nodes = doc.xpathEval(self.xpath)
 		nodes = doc.xpathEval(self.xpath)
 		return [TargetInfo(self, node.content) for node in nodes]
 
@@ -112,78 +154,106 @@ class HtmlFileGenerator:
 		file = open(outfile, 'w')
 		file.write(html)
 		file.close()
-
+		
+class TargetPair:
+	def __init__(self, source, proc):
+		self.source = source
+		self.proc = proc
+		
 class TargetProcessor:
-	def __init__(self, linkfmt, actionLinkfmt, procRecFile, spacechar, srcTe, procTe, actionResult):
+	def __init__(self, linkfmt, actionLinkfmt, procRecFile, spacechar, srcTe, procTe, actionResult, log=LoggerFactory().empty()):
+		self.log = log
 		self.linkfmt = linkfmt
 		self.actionLinkfmt = actionLinkfmt
 		self.procRecFile = procRecFile
 		self.spacechar = spacechar
-		self.downloader = HttpDownloader()
 		self.srcTe = srcTe
 		self.procTe = procTe
 		self.actionResult = actionResult
+		self.downloader = HttpDownloader(log=log)
 
-	def process(self, feedUrl):		
-		# download the feed xml
-		feedXml = self.downloader.download(feedUrl)
+	def process(self, feedUrl):
+		try:
+			self.log.debug('processing feed')
 		
-		# create some easy to use target objects
-		srcTargets = self.srcTe.getTargets(feedXml)
-		
-		# for each source target, get a target to process 
-		procTargets = self.getProcTargets(srcTargets)
-		
-		# actions each proc target assiming they are urls
-		return self.handleProcTargets(procTargets)
+			# download the feed xml
+			feedXml = self.downloader.download(feedUrl)
+			
+			# create some easy to use target objects
+			srcTargets = self.srcTe.getTargets(feedXml)
+			self.log.debug('source targets: %i' % len(srcTargets))
+			
+			# for each source target, get a target to process 
+			procTargets = self.getProcTargets(srcTargets)
+			self.log.debug('targets to process: %i' % len(procTargets))
+			
+			# action each proc target assiming they are urls
+			procCount = self.handleProcTargets(procTargets)
+			self.log.info('processed targets: %i', procCount);
 	
-	def handleProcTargets(self, procTargets):		
-		# get a list of formatted processed target strings
-		targetStrings = [pt.getString('{0}') for pt in procTargets]
-		
-		# get a list of already processed targets
-		procRecord = self.getprocRecord()
+		except KeyboardInterrupt:
+			self.log.warning('user aborted')
+	
+	def handleProcTargets(self, procTargets):
 		
 		handled = 0
-		for ts in targetStrings:
+		for pt in procTargets:
 			
-			# only process targets that haven't been processed yet
-			if ts not in procRecord:
-				
-				# get the action url based on the target url
-				actionUrl = self.actionLinkfmt % urllib.quote(ts)
-				
+			# get formatted string to process
+			ts = pt.proc.getString('{0}')
+			
+			# get the action url based on the target url
+			actionUrl = self.actionLinkfmt % urllib.quote(ts)
+			
+			try:
 				# go ahead and action the url
 				result = self.downloader.download(actionUrl).rstrip('\n')
-				
+			
 				if result == self.actionResult:
 					# once the target has ben processed ok
-					self.appendProcRec(ts)
+					self.appendProcRec(pt.source)
 					handled += 1
 				else:
-					print 'Result: ' + result
-					print "Error: Did not get expected result (%s)." % self.actionResult
+					self.log.warning('result: %s' % result)
+					self.log.error('did not get expected result (%s).' % self.actionResult)
+					
+			except Exception, e:
+				self.log.error('unable to download: %s' % e)
 		
 		return handled
 	
-	def getProcTargets(self, srcTargets):		
+	def getProcTargets(self, srcTargets):
+		# get a list of already processed targets
+		procRecord = self.getProcRecord()
+		
 		procTargets = list()
 		for source in srcTargets:
-			feedUrl = source.getString(self.linkfmt, self.spacechar)
+		
+			# construct a url from the source
+			sourceUrl = source.getString(self.linkfmt, self.spacechar)
 			
-			# assume that each target will get us a link to an xml feed
-			xml = self.downloader.download(feedUrl)
-			
-			# get some sub-targets as can be found in the xml
-			subTargets = self.procTe.getTargets(xml)
-			
-			if len(subTargets) > 0:
-				# but only use the first (if there are any at all)
-				procTargets.append(subTargets[0])
+			# make sure we don't re-run already successful source urls
+			if sourceUrl not in procRecord:
+				
+				# assume that each target will get us a link to an xml feed
+				xml = self.downloader.download(sourceUrl)
+				
+				# get some sub-targets as can be found in the xml
+				subTargets = self.procTe.getTargets(xml)
+				
+				if len(subTargets) > 0:
+					# but only use the first (if there are any at all)
+					procTargets.append(TargetPair(sourceUrl, subTargets[0]))
+			else:
+				self.log.debug(
+					'ignoring already successful source, %s (%s)' % (
+						' - '.join(source.parts), sourceUrl
+					)
+				)
 		
 		return procTargets
 	
-	def getprocRecord(self):
+	def getProcRecord(self):
 		if os.path.exists(self.procRecFile):
 			doc = libxml2.parseFile(self.procRecFile)
 			nodes = doc.xpathEval('/procrec/target')
